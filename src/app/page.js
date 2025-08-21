5351137d94671ba16c2c1fd1ed950d0a723d26ba
piipeares <piipeares@gmail.com>
2025-08-21 20:12:40 -0300

    agregados home general

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HomeContainer from "@/components/HomeContainer";
 
-export default function Home() {
-  return (
-    <div>
-      Hola
-    </div>
-  );
+export default async function Page() {
+  // api real
+  const res = await fetch("https://miapi.com/products", { cache: "no-store" });
+  const products = await res.json();
+
+  return <HomeContainer products={products} />;
 }
